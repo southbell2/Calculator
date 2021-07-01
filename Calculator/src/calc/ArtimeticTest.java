3f733b1a0fb2382f61537fdbdeb055199093aba1
southbell2 <southbell2357@naver.com>
2021-07-02 03:11:52 +0900

    first commit

--- Calculator/src/calc/ArtimeticTest.java
+++ Calculator/src/calc/ArtimeticTest.java
@@ -5,12 +5,16 @@ public class ArtimeticTest {
 	public static void main(String[] args) {
 		String test1 = "3+4*5-7";
 		String test2 = "1+2+3";
+		String test3 = "2*3+";
 		
 		Arithmetic testCalc = new Arithmetic();
 		testCalc.setString(test1);
 		System.out.println(testCalc.calculate());
 		
 		testCalc.setString(test2); 
+		System.out.println(testCalc.calculate());
+		
+		testCalc.setString(test3); 
 		System.out.println(testCalc.calculate());
 		 
 	}
